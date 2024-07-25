ccfbf03b8b2d4e5b77d92b02afa8946c4dc6fd82
AKHIL-JOSE-10 <akhiljosetcr@gmail.com>
2024-07-25 20:55:34 +0530

    navebar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,15 @@ const Navbar = () => {
     <div className="nav">
       <input type="checkbox" id="nav-check" />
       <div className="nav-header"></div>
+      <div className="nav-title">
+        STUDENT
+      </div>
       <div className="nav-btn"></div>
+      <label htmlFor="nav-check">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
 
       <div className="nav-links"></div>
     </div>
