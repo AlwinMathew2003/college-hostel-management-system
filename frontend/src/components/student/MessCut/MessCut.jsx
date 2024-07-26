f427c12a9588f9b803d505db2d262899da97aaf6
Akhil-jose-10 <akhiljosetcr@gmail.com@gmail.com>
2024-07-26 14:11:49 +0530

    motion

--- frontend/src/components/student/MessCut/MessCut.jsx
+++ frontend/src/components/student/MessCut/MessCut.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./MessCut.css";
+import {motion} from 'framer-motion'
 
 const MessCut = () => {
   return (
     <div className="mess-cut-main">
-      <h1 className="mess-cut-heading" style={{ fontSize: "50px", color: "#333", fontFamily: "cursive", textShadow: "2px 2px 2px #ccc" }}>Mess Cut</h1>
-      <form >
+      <h1
+      className="mess-cut-heading" style={{ fontSize: "50px", color: "#333", fontFamily: "cursive", textShadow: "2px 2px 2px #ccc" }}>Mess Cut</h1>
+            <motion.form
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1 }}
+        action="/submit-mess-cut-permission"
+        method="post"
+      >
         <div>
           <label htmlFor="admissionNumber">Admission NO:</label>
           <input type="text" id="admissionNumber" name="admissionNumber" required />
@@ -18,7 +26,7 @@ const MessCut = () => {
           <label htmlFor="name">Name:</label>
           <input type="text" id="name" name="name" required />
         </div>
-      </form>
+      </motion.form>
     </div>
   );
 };
