0dda1ecfffdf2e6a11aaff82f7e3c8c11468f0cd
AKHIL-JOSE-10 <akhiljosetcr@gmail.com>
2024-07-27 18:29:02 +0530

    css added

--- frontend/src/components/student/ComplaintForm/ComplaintForm.jsx
+++ frontend/src/components/student/ComplaintForm/ComplaintForm.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "./ComplaintForm.css";
+import {motion} from 'framer-motion'
 
 const ComplaintForm = () => {
   return (
     <div className="complaint-form-container">
       <h1 className="complaint-form-heading"  style={{ fontSize: "50px", color: "#333", fontFamily: "cursive", textShadow: "2px 2px 2px #ccc" }}>Complaint Form</h1>
       <form >
-        <div className="complaint-form-group">
+        <motion.div 
+         whileInView={{ opacity: 1, x: 0 }}
+         initial={{ opactiy: 0, x: -100 }}
+         transition={{ duration: 0.9 }}
+
+        className="complaint-form-group">
+          
           <label htmlFor="complain">Complaint:</label>
           <textarea id="complain" name="complain" rows="5" required></textarea>
-        </div>
+        </motion.div>
         <button type="submit" className="complaint-form-submit">SUBMIT</button>
       </form>
     </div>
