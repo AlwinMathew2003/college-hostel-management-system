ea1507caf3f768536fa6f1666c01ecf1f9e8d217
Gklf5 <adithya.eg5@gmail.com>
2024-08-03 11:58:37 +0530

    setStart

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 // Backend server
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv')
-const mongoose = require("mongoose")
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
@@ -12,15 +12,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.set('debug', true);
-mongoose.connect('mongodb+srv://akhiljosetcr:kR1i0eZiPMZBqZGl@college-hostel.daqchog.mongodb.net/?retryWrites=true&w=majority&appName=College-Hostel')
-.then(()=>{
-console.log("MongoDB Connected")
-})
-.catch((err)=>{
-    console.log(err)
-})
+mongoose.set("debug", true);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(PORT,()=>{
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
