7074e96ce2d8ce79e87f779ccdeaf1dff0b6057a
AlwinMathew2003 <alwinchungath@gmail.com>
2024-10-29 10:42:16 +0530

    Mysql Configuration

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ import complaintRoutes from "./routes/student/complaintRoutes.js";
 import messcutpermissionRoutes from "./routes/student/messcutpermissionRoutes.js";
 import apologyRoutes from "./routes/admin/apologyRoutes.js"
 import passwordRoutes from "./routes/student/updatePassword.js"
+import mysql from "mysql2";
+
 
 config();
 
@@ -19,14 +21,33 @@ const app = express();
 app.use(json());
 app.use(cors());
 
-set("debug", true);
-connect(process.env.MONGO)
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// // set("debug", true);
+// // connect(process.env.MONGO)
+// //   .then(() => {
+// //     console.log("MongoDB Connected");
+// //   })
+// //   .catch((err) => {
+// //     console.log(err);
+// //   });
+
+// Create a connection pool
+const db  = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'hostel'
+});
+
+// Test the connection
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL!');
+  connection.release(); // Release the connection back to the pool
+});
+
 
 app.use("/api/login", authUser);
 app.use("/api/complaints",complaintRoutes )
