d647ee722719cc0ecddef42b92d9f2eb1ea5ffdb
AlwinMathew2003 <alwinchungath@gmail.com>
2024-10-30 00:10:09 +0530

    Updated Login

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,10 +5,11 @@ export const signin = async (req, res) => {
     console.log(req.body);
 
     // Query for the user in the login table based on the provided username
-    const [rows] = await db.execute(
-      "SELECT * FROM login WHERE username = ?",
-      [req.body.admno]
-    );
+    const [rows] = await db.execute("SELECT * FROM login WHERE username = ?", [
+      req.body.admno,
+    ]);
+
+    console.log(rows.length);
 
     // Check if the user was found
     if (rows.length > 0) {
@@ -18,34 +19,35 @@ export const signin = async (req, res) => {
       if (user.password === req.body.password) {
         console.log("User authenticated:", user);
         const [studentResults] = await db.execute(
-          'SELECT * FROM student WHERE admno = ?',
+          "SELECT * FROM student WHERE admno = ?",
           [user.username]
         );
 
         if (studentResults.length === 0) {
-          return res.status(404).json({ error: 'Student details not found' });
+          return res.status(404).json({ error: "Student details not found" });
         }
-    
+
         const student = studentResults[0];
         const modifiedStudentData = {
-          adm_no: student.admno,   // Renamed from `admno` to `studentId`
-          name: student.name,         // Keeping `name` the same
+          adm_no: student.admno, // Renamed from `admno` to `studentId`
+          name: student.name, // Keeping `name` the same
           Room_no: student.room_no, // Renamed from `room_no` to `roomNumber`
           department: student.department,
           sem: student.sem,
           phone_no: student.phone_no,
           guardian_name: student.guardian_name,
           guardian_no: student.guardian_no,
-          jecc_mail: student.jecc_mail.trim() // Trim any trailing newline characters
+          jecc_mail: student.jecc_mail.trim(), // Trim any trailing newline characters
         };
-        
+
         console.log(student);
-        console.log(modifiedStudentData)
+        console.log(modifiedStudentData);
         res.json(modifiedStudentData); // Send user info without password
       } else {
         res.status(401).json({ message: "Incorrect password" }); // Unauthorized
       }
     } else {
+      console.log("User not found")
       res.status(404).json({ message: "User not found" }); // Not found
     }
   } catch (error) {
@@ -69,4 +71,3 @@ export const fetchUsers = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
-
